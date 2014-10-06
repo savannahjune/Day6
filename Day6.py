3a9a006d990106bb10b5a466a580e5c556275501
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 12:20:42 -0700

    dictionary making module

--- Day6.py
+++ Day6.py
@@ -1,62 +1,39 @@
-# create a mapping of state to abbreviation
-states = {
-    'Oregon': 'OR',
-    'Florida': 'FL',
-    'California': 'CA',
-    'New York': 'NY',
-    'Michigan': 'MI'
-}
+def num(num_buckets=256):
+    """Initializes a Map with the given number of buckets."""
+    aMap = []
+    for i in range(0, num_buckets):
+        aMap.append([])
+    return aMap
 
-# creates a basic set of states and some cities in them
-cities = {
-    'CA':'San Francisco',
-    'MI':'Detroit',
-    'FL':'Jacksonville'
-}
+def hash_key (aMap, key ):
+    """Given a key this will create a number and then convert it to
+    an index for the aMap's buckets."""
+    return hash(key) % len(aMap)
 
-# add some more cities
-cities['NY'] = 'New York'
-cities['OR'] = 'Portland'
+def get_bucket(aMap, key):
+    """Given a key, find the bucket where it would go."""
+    bucket_id = hash_key(aMap, key)
+    return aMap[bucket_id]
 
-# print out some cities
-print '-' * 10
-print "NY State has: ", cities['NY']
-print "Or State has: ", cities['OR']
+def get_slot(aMap, key, default=None):
+    """
+    Returns the index, key, and value of a slot found in a bucket.
+    Returns -1, key, and default (None if not set) when not found.
+    """
+    bucket = get_bucket(aMap, key)
 
-# print some states
-print '-' * 10
-print "NY State has: ", cities['NY']
-print "OR State has: ", cities['OR']
+    for i, kv in enumerate (bucket):
+        k,v = kv
+        if key == k:
+            return i, k, v
 
-#do it by using the state then cities dict
-print '-' * 10
-print "Michigan has: ", cities[states['Michigan']]
-print "Florida has: ", cities[states["Florida"]]
+    return -1, key, default
 
-#print every state abbreviation
-print '-' * 10
-for state, abbrev in states.items():
-    print "%s is abbreviated %s" % (state, abbrev)
+def get(aMap, key, default=None):
+    """Gets the value in a bucket for the given key, or the default."""
+    i, k, v = get_slot (aMap, key, default=default)
+    return v
 
-# print every city in state
-print '-' * 10
-for abbrev, city in cities.items():
-    print "%s has the city %s" % (abbrev, city)
-
-#now do both at the same time
-print '-' * 10
-for state, abbrev in states.items():
-    print "%s state is abbreviated %s and has city %s" % (
-        state, abbrev, cities[abbrev])
-
-print '-' * 10
-# safely get a abbreviation by state that might not be there
-state = states.get('Texas')
-
-if not state:
-    print "Sorry, no Texas."
-
-# get a city with a default value
-city = cities.get('TX', 'Does Not Exist')
-print "The city for the state TX is: %s" % city
- 
+def set (aMap, key, value)
+"""Sets the key to the value, replacing any existing value."""
+    
