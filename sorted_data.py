af0cf6051595abf2925ad0219af5229847540d30
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 16:19:59 -0700

    finished exercise 6

--- sorted_data.py
+++ sorted_data.py
@@ -1,11 +1,14 @@
 def main():
-    sortedscores = {}
+    sorted_scores = {}
     scores = open('scores.txt')
     for line in scores:
         line = line.rstrip()
         line = line. split(':')
-        sortedscores[line[0]] = line[1]
-    print sortedscores
+        sorted_scores[line[0]] = line[1]
+        sorted_scores_keys = sorted(sorted_scores.keys())
+    for key in sorted_scores_keys:
+        value = sorted_scores[key]
+        print "Restaurant %s is rated at %s" % (key, value)  
 
 if __name__ == '__main__':
     main()
